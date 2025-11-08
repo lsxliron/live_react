260da50bab3f754c4cbd63c36e79ee051e7ef396
lsxliron <lsxliron@noreply.com>
2025-11-08 18:20:07 -0500

    fix: updated  example app to use new configurations
    Signed-off-by: lsxliron <lsxliron@noreply.com>

--- live_react_examples/assets/vite.config.js
+++ live_react_examples/assets/vite.config.js
@@ -5,6 +5,38 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import liveReactPlugin from "live_react/vite-plugin";
 
+const build_env = process.env.BUILD_ENV == "production" ? "prod" : "dev";
+const PHX_APP_NAME =  path.basename(path.resolve(".."))
+
+// returns no ecternals for prod SSR build and externals for dev SSR build
+function getSsrExternals(isDev) {
+  if (isDev) {
+    return {
+      external: ["react", "react-dom"],
+      noExternal: ["live_react"],
+    };
+  }
+  return {};
+}
+
+// returns aliases for prod build to avoid duplicate react copies
+function getAliases(isDev) {
+  if (!isDev) {
+    const aliases = {
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+    };
+
+    // required for phoenix 1.8 and up
+    aliases[`phoenix-colocated/${PHX_APP_NAME}`] = path.resolve(
+      __dirname,
+      `../_build/${build_env}/phoenix-colocated/${PHX_APP_NAME}`,
+    )
+    return aliases
+  }
+  return {};
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const isDev = command !== "build";
@@ -20,10 +52,12 @@ export default defineConfig(({ command }) => {
     ssr: {
       // we need it, because in SSR build we want no external
       // and in dev, we want external for fast updates
-      noExternal: isDev ? undefined : true,
+      ...getSsrExternals(isDev),
     },
     resolve: {
+      preserveSymlinks: true,
       alias: {
+        ...getAliases(isDev),
         "@": path.resolve(__dirname, "."),
       },
     },
